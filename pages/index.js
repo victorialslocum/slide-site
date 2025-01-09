4f217ced4efa140722534ba2d54971b1de6182dc
Victoria Slocum <victorialslocum@gmail.com>
2025-01-09 19:33:51 +0100

    updates

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@
 import Link from "next/link";
 import path from "path";
 import fs from "fs/promises";
+import Image from "next/image";
+const Home = ({ decks }) => {
 
-const Home = ({ decks, previews }) => {
   return (
     <div>
       <h1 className="text-4xl font-bold mb-4">Slides Slides Slides</h1>
@@ -12,16 +13,10 @@ const Home = ({ decks, previews }) => {
         Collection of my slides from various presentations. 
       </p>
 
-      <div className="flex flex-row">
+      <div className="grid grid-cols-3">
         {decks.map((deck, index) => (
           <div className="bg-gray-100 basis-1/3 m-4 p-4 rounded-lg shadow-md justify-center" key={deck}>
-            <iframe
-              srcDoc={previews[index]}
-              width="400px"
-              height="300px"
-              title={`Preview of ${deck}`}
-              sandbox="allow-scripts allow-same-origin"
-            />
+            <Image src={`/preview/${encodeURIComponent(deck)}.png`} alt={deck} width={400} height={300} loading="lazy" className="rounded-md" />
             <h2 className="text-2xl font-bold my-2 text-center">{deck}</h2>
             <div className="flex justify-center">
               <Link href={`slides/${encodeURIComponent(deck)}/index.html`}>
@@ -41,27 +36,9 @@ export async function getStaticProps() {
   const decksDirectory = path.join(process.cwd(), "public/slides");
   const decks = await fs.readdir(decksDirectory);
 
-  // Generate previews during the build process
-  const previews = await Promise.all(
-    decks.map(async (deck) => {
-      try {
-        const indexPath = path.join(decksDirectory, deck, "index.html");
-
-        let rawContent = await fs.readFile(indexPath, "utf-8");
-        let newContent = rawContent.replace(/lib\//g, "slides/" + String(deck) + "/lib/");
-        let content = newContent.replace(/assets\//g, "slides/" + String(deck) + "/assets/");
-        return content;
-      } catch (error) {
-        console.error(`Error reading ${deck}/index.html:`, error);
-        return null;
-      }
-    })
-  );
-
   return {
     props: {
       decks,
-      previews,
     },
   };
 }
