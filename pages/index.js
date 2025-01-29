ef58b57eb1d78b61e717b97e816da5ea4b3a03b4
Victoria Slocum <victorialslocum@gmail.com>
2025-01-29 22:41:45 +0100

    update slugs

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const Home = ({ decks }) => {
         {decks.map((deck, index) => (
           <div className="bg-gray-100 basis-1/3 m-4 p-4 rounded-lg shadow-md justify-center" key={deck}>
             <Image src={`/preview/${encodeURIComponent(deck)}.png`} alt={deck} width={400} height={300} loading="lazy" className="rounded-md" />
-            <h2 className="text-2xl font-bold my-2 text-center">{deck}</h2>
+            <h2 className="text-2xl font-bold my-2 text-center">{deck.replace(/-/g, ' ')}</h2>
             <div className="flex justify-center">
               <Link href={`slides/${encodeURIComponent(deck)}/index.html`}>
                 <button className="bg-cyan-400 text-white p-3 mx-4 my-2 mr-2 rounded shadow-md">
